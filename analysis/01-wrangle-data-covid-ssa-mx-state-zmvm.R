ba4bc8595511cfa91d2445476ecd050351847c94
MarianaFdz <marianafdz465@gmail.com>
2020-06-13 20:48:54 -0500

    Raw and clean Data from SSA for June 13th

--- analysis/01-wrangle-data-covid-ssa-mx-state-zmvm.R
+++ analysis/01-wrangle-data-covid-ssa-mx-state-zmvm.R
@@ -21,7 +21,7 @@ library(tidyverse)
 ####            Load data                 #### 
 #--------------------------------------------#
 
-ssa <- read.csv("data-raw/200612COVID19MEXICO.csv")
+ssa <- read.csv("data-raw/200613COVID19MEXICO.csv")
 load("data-raw/df_pop_state.Rdata")   # population for states
 # Data for ZMVM
 load("data-raw/df_pop_ZMVM.Rdata")   
