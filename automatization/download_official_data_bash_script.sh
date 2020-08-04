7f0d85a33c8b3552eb45e622f434b5d15ef1f38a
marianafdz465 <56494627+marianafdz465@users.noreply.github.com>
2020-08-03 22:10:07 -0500

    Update download_official_data_bash_script.sh

--- automatization/download_official_data_bash_script.sh
+++ automatization/download_official_data_bash_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/Users/marianafernandez/miniconda3/bin/python3 /Users/marianafernandez/Documents/COSMO/covid19-mx-data/automatization/download_official_data.py
+/Users/miniconda3/bin/python3 Documents/COSMO/covid19-mx-data/automatization/download_official_data.py
