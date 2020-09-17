71af8212dfc48fe36174c4ee26674118a15ef1dc
MarianaFdz <marianafdz465@gmail.com>
2020-09-16 21:54:43 -0500

    Raw and Clean Data from SSA for September 16th

--- analysis/01-wrangle-data-covid-ssa-mx-state.R
+++ analysis/01-wrangle-data-covid-ssa-mx-state.R
@@ -21,7 +21,7 @@ library(data.table)
 #--------------------------------------------#
 ####            Load data                 #### 
 #--------------------------------------------#
-ssa <- fread("data-raw/200915COVID19MEXICO.csv",header=TRUE)
+ssa <- fread("data-raw/200916COVID19MEXICO.csv",header=TRUE)
 #ssa <- read.csv("data-raw/200717COVID19MEXICO.csv")
 
 load("data-raw/df_pop_state.Rdata")   # population for states
