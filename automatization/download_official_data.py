c38bb39d0aeec77df0fc93f476a1833cd77fc1b2
MarianaFdz <marianafdz465@gmail.com>
2020-12-09 21:32:09 -0600

    Raw and Clean Data from SSA for December 9

--- automatization/download_official_data.py
+++ automatization/download_official_data.py
@@ -6,6 +6,7 @@ from io import StringIO
 
 import requests, zipfile, io
 zip_file_url = "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
+zip_file_url = "http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
 r = requests.get(zip_file_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall(path = '/Users/Documents/COSMO/covid19-mx-data/data-raw/')
+z.extractall(path = '/Users/marianafernandez/Documents/PADeCI/covid19-mx-data/data-raw/')
