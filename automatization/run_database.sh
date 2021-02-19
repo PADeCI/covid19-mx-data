262fa2c40b22ecee67a4012a4e25d53de23c1922
admon <marianafdz465@gmail.com>
2021-02-18 21:52:07 -0600

    Automate database

---
+++ automatization/run_database.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python3 download_official_data.py
+
+echo "####################################"
+echo "BASE DESCARGADA EXITOSAMENTE DE SSA"
+echo "###################################"
+
+Rscript hello.R
+ 
+echo "###################################"
+echo "BASE ACTUALIZADA EXITOSAMENTE" 
+echo "##################################" 
