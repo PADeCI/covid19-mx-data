1e8fffa901fa04411346433372253a1e29040319
marianafdz465 <marianafdz465@gmail.com>
2021-04-25 21:34:38 -0500

    Raw and Clean Data from SSA for April 25

--- automatization/download_official_data.py
+++ automatization/download_official_data.py
@@ -4,15 +4,22 @@ import csv
 import urllib.request
 from io import StringIO
 
-from datetime import datetime
 import requests, zipfile, io
+import os
 
+from datetime import date
+
+os.chdir("/home/admon/covid19-mx-data/data-raw")
 today = date.today()
-d1 = today.strftime("%d%m%Y")
+d1 = today.strftime("%y%m%d")
+file_name = d1 + "COVID19MEXICO.csv"
 
 #zip_file_url = "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
 zip_file_url = "http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
 r = requests.get(zip_file_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 #z.extractall(path = '/home/admon/covid19-mx-data/data-raw')
-z.extractall(name = d1, path = '/home/admon/covid19-mx-data/data-raw')
+z.extractall(path = '/home/admon/covid19-mx-data/data-raw')
+
+#Rename csv file since april 25
+os.rename("COVID19MEXICO.csv",file_name)
