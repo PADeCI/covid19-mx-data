ec79666626cb22fee127cfc7c4cf48fa94a9f7e4
marianafdz465 <marianafdz465@gmail.com>
2021-05-28 07:29:49 -0500

    'Raw and Clean Data from SSA for '

--- automatization/run_database.sh
+++ automatization/run_database.sh
@@ -16,8 +16,8 @@ Rscript "/home/admon/covid19-mx-data/main.R"
 echo "###################################"
 echo "BASE ACTUALIZADA EXITOSAMENTE" 
 echo "##################################" 
-
-date=$(date +"%Y-%m-%d")
+sum=1
+date=$(date +"%Y-%m-%d" -$s)
 
 message="Raw and Clean Data from SSA for "
 final_message=$message$date
